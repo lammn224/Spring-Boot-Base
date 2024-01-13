557cf0016bf00adaef65cb413a15d7866a5a6bcb
lammn224 <hoanglam2242001@gmail.com>
2024-01-13 23:58:43 +0700

    feat: commanline runner

--- src/main/java/com/lammai/SpringBootBase/SpringBootBaseApplication.java
+++ src/main/java/com/lammai/SpringBootBase/SpringBootBaseApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootBaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBaseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBaseApplication.class, args);
+    }
 
+//	@Bean
+//	CommandLineRunner run(UserService userService) {
+//		return args -> {
+//			CreateUserDto createAdmin = new CreateUserDto("ngoclam.work@gmail.com", "12345678", "lammn2", Role.ADMIN);
+//			CreateUserDto createUser = new CreateUserDto("hoanglam2242001@gmail.com", "12345678", "ngoclam", Role.USER);
+//
+//			userService.createUser(createAdmin);
+//			userService.createUser(createUser);
+//		};
+//	}
 }
